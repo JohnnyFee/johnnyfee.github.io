c197fd1624fe1c38ad06731cf324a5eae32fe4a7
feiq <feiq@landicorp.com>
2018-08-19 17:38:31 +0800

    rm util

--- utils/Get-Posts.cmd
+++
@@ -1,1 +0,0 @@
-@set Path=%Path%;%SystemRoot%\system32\WindowsPowerShell\v1.0\ & powershell -ExecutionPolicy Unrestricted -NoExit -NoProfile %~dpn0.ps1
--- utils/qiniu-sync-images.cmd
+++
@@ -1,3 +0,0 @@
-echo off
-rem ![](http://johnnyimages.qiniudn.com/hack1-center.png)
-%~dp0\qiniu\qrsync %~dp0\..\qiniu-config.json
